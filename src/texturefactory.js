ce015aab615c0b262cb7f394284a64ae069a7f66
daishihmr <daishi.hmr@gmail.com>
2015-07-17 15:09:00 +0900

    fix

--- src/texturefactory.js
+++ src/texturefactory.js
@@ -13,8 +13,8 @@
         canvas.width = 256;
         canvas.height= 1;
         var context = canvas.getContext("2d");
-        for (var i = 0, len = palette.length; i < len; i++) {
-            var p = palette[i];
+        for (var i = 0, len = voxelData.palette.length; i < len; i++) {
+            var p = voxelData.palette[i];
             context.fillStyle = "rgb(" + p.r + "," + p.g + "," + p.b + ")";
             context.fillRect(i * 1, 0, 1, 1);
         }
